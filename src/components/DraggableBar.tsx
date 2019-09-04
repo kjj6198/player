800925e48be8eb43369c8a36a9241ca95db9606f
kjj6198 <kjj6198@gmail.com>
2019-09-04 14:43:55 +0900

    Add DraggableBar and rxjs support

--- src/components/DraggableBar.tsx
+++ src/components/DraggableBar.tsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useRef, MouseEvent } from 'react';
-import styled, { StyledComponent } from 'styled-components';
-import {
-  fromEvent,
-  Observable,
-  Subscription,
-  animationFrameScheduler
-} from 'rxjs';
+import React, { createRef } from 'react';
+import styled from 'styled-components';
+import { fromEvent, Subscription, animationFrameScheduler } from 'rxjs';
 import {
   map,
   merge,
@@ -15,10 +10,10 @@ import {
   startWith,
   tap,
   filter,
-  observeOn
+  observeOn,
 } from 'rxjs/operators';
 
-const BarComponent = styled.progress`
+const BarComponent = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
@@ -30,21 +25,22 @@ const BarComponent = styled.progress`
 
 export const Indicator = styled.button.attrs<{ current: number }>(props => ({
   style: {
-    left: `${props.current * 100}%`
-  }
-}))`
+    left: `${props.current * 100}%`,
+  },
+}))<{ current: number }>`
   position: absolute;
-  top: -4px;
+  z-index: 1;
+  top: -11px;
   left: 0;
-  width: 15px;
-  height: 15px;
+  width: 30px;
+  height: 30px;
   border-radius: 50%;
-  appearance: none;
-  curosr: pointer;
-  transform: translate(-7.5px);
+  border: 1px solid #ddd;
+  transform: translateX(-15px);
   transform-origin: center;
   background-color: #fff;
   transition: box-shadow 0.2s ease;
+  cursor: pointer;
   &:focus {
     outline: none;
     box-shadow: 0 0 5px 5px rgba(200, 200, 200, 0.5);
@@ -53,16 +49,16 @@ export const Indicator = styled.button.attrs<{ current: number }>(props => ({
 
 export const ProgressFill = styled.div.attrs<{ current: number }>(props => ({
   style: {
-    width: `${(props.current || 0) * 100}%`
-  }
-}))`
+    width: `${(props.current || 0) * 100}%`,
+  },
+}))<{ current: number }>`
   pointer-events: none;
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
   height: 100%;
-  background-color: #fff;
+  background-color: rgba(60, 60, 60, 0.8);
   left: 0;
   border-radius: 5px;
 `;
@@ -72,51 +68,49 @@ type Props = {
   setCurrent: (current: number) => void;
 };
 
-const Bar: React.FC<Props> = ({ current, setCurrent }) => {
-  const barRef = useRef<HTMLProgressElement>();
-  const indicatorRef = useRef<HTMLButtonElement>();
-  const slide$ = useRef<Subscription | null>(null);
+export default class Bar extends React.Component<Props> {
+  barRef = createRef<HTMLDivElement>();
+  indicatorRef = createRef<HTMLButtonElement>();
+  slide$: Subscription;
 
-  useEffect(() => {
-    if (barRef.current && indicatorRef.current) {
-      const width = barRef.current.offsetWidth;
-      slide$.current = fromEvent(barRef.current, 'mousedown')
-        .pipe(
-          merge(
-            fromEvent(indicatorRef.current, 'mousedown').pipe(
-              map((e: MouseEvent) => ({
-                pageX: e.pageX,
-                offsetX: current * width
-              }))
-            )
+  componentDidMount() {
+    const { offsetWidth: width } = this.barRef.current;
+    const { setCurrent } = this.props;
+    this.slide$ = fromEvent(this.barRef.current, 'mousedown')
+      .pipe(
+        merge(
+          fromEvent<MouseEvent>(this.indicatorRef.current, 'mousedown').pipe(
+            map(({ pageX }) => ({
+              pageX,
+              offsetX: this.props.current * width,
+            })),
           ),
-          throttleTime(100),
-          switchMap(({ offsetX: initialX, pageX: x }) =>
-            fromEvent(window, 'mousemove').pipe(
-              map(e => (initialX + e.pageX - x) / width),
-              /* make button blur when mouse up */
-              takeUntil(
-                fromEvent(window, 'mouseup').pipe(
-                  tap(
-                    () =>
-                      document.activeElement && document.activeElement.blur()
-                  )
-                )
-              ),
-              startWith(initialX / width)
-            )
-          ),
-          filter(progress => progress <= 1 || progress >= 0),
-          observeOn(animationFrameScheduler)
-        )
-        .subscribe(setCurrent);
-    }
-  }, []);
+        ),
+        throttleTime(100),
+        switchMap(({ offsetX: initialX, pageX: x }: MouseEvent) => fromEvent<MouseEvent>(window, 'mousemove').pipe(
+          map(e => (initialX + e.pageX - x) / width),
+          /* make button blur when mouse up */
+          takeUntil(fromEvent(window, 'mouseup')
+            .pipe(tap(() => document.activeElement instanceof HTMLElement && document.activeElement.blur()))),
+          startWith(initialX / width),
+        )),
+        filter(progress => progress <= 1 || progress >= 0),
+        observeOn(animationFrameScheduler),
+      )
+      .subscribe(setCurrent);
+  }
 
-  return (
-    <BarComponent ref={barRef}>
-      <ProgressFill current={current} />
-      <Indicator ref={indicatorRef} current={current} />
-    </BarComponent>
-  );
-};
+  componentWillUnmount() {
+    this.slide$.unsubscribe();
+  }
+
+  render() {
+    const { current } = this.props;
+    return (
+      <BarComponent ref={this.barRef}>
+        <ProgressFill current={this.props.current} />
+        <Indicator ref={this.indicatorRef} current={current} />
+      </BarComponent>
+    );
+  }
+}
