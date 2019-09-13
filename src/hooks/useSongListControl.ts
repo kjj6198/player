b5f3617cfaa67560a060c42a5f80e93d4a640a06
kjj6198 <kjj6198@gmail.com>
2019-09-13 10:51:42 +0900

    add useSongListControl to hook

--- src/hooks/useSongListControl.ts
+++ src/hooks/useSongListControl.ts
@@ -3,32 +3,37 @@ import { AppContext } from '../context/AppContext';
 
 export default function useSongListControl() {
   const { state, setContext } = useContext(AppContext);
-  const handleSongListOperation = useCallback((btnId: 'next' | 'prev') => {
-    return () => {
-      const idx = Array.from(state.songs).findIndex(song => song.name === state.current.name);
-      if (btnId === 'next') {
-        if (idx === state.songs.length - 1) {
-          setContext({
-            current: state.songs[0],
-          });
-        } else {
-          setContext({
-            current: state.songs[idx + 1],
-          });
+  const handleSongListOperation = useCallback(
+    (btnId: 'next' | 'prev') => {
+      return () => {
+        const idx = Array.from(state.songs).findIndex(
+          song => song.name === state.current.name
+        );
+        if (btnId === 'next') {
+          if (idx === state.songs.length - 1) {
+            setContext({
+              current: state.songs[0],
+            });
+          } else {
+            setContext({
+              current: state.songs[idx + 1],
+            });
+          }
+        } else if (btnId === 'prev') {
+          if (idx === 0) {
+            setContext({
+              current: Array.from(state.songs).slice(-1)[0],
+            });
+          } else {
+            setContext({
+              current: Array.from(state.songs)[idx - 1],
+            });
+          }
         }
-      } else if (btnId === 'prev') {
-        if (idx === 0) {
-          setContext({
-            current: Array.from(state.songs).slice(-1)[0]
-          })
-        } else {
-          setContext({
-            current: Array.from(state.songs)[idx - 1]
-          })
-        }
-      }
-    }
-  }, [setContext, state]);
+      };
+    },
+    [setContext, state]
+  );
 
   return handleSongListOperation;
 }
